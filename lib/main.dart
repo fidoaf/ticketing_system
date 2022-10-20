5f42e0b17e401fa5e5ab38c8ab705c3d7a930c04
fidoaf <jartesfigueres@gmail.com>
2022-10-20 12:34:07 +0200

    Fix imports
    Using flutter_bloc instead of bloc (Dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ticketing_system/app.dart';
 import 'package:ticketing_system/section/section_repository.dart';
 import 'package:ticketing_system/simple_bloc_observer.dart';
--- lib/section/section_bloc.dart
+++ lib/section/section_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ticketing_system/section/section_repository.dart';
 
